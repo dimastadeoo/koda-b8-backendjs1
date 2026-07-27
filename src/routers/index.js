c8ca7ec4220e2d0fc098765d9ba3c8f99bdecc6c
dimastadeoo <dimastadeoo@gmail.com>
2026-07-27 14:16:21 +0700

    feat: add auth router to main router for authentication endpoints

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,11 @@
 import {Router} from "express"
 import userRouter from "./users.router.js"
+import authRouter from "./auth.router.js"
 
 const router = Router()
 
 router.use("/users", userRouter)
+router.use("/auth", authRouter)
+
 
 export default router
