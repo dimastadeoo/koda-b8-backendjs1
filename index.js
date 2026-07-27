d2cfb6de8b6cee671ce0e060e80ae5025314b562
dimastadeoo <dimastadeoo@gmail.com>
2026-07-27 20:15:22 +0700

    feat: add qs library for query string parsing and update dependencies

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express"
 import router from "./src/routers/index.js"
+import qs from "qs"
 // import {constants} from "node:http2"
 // import auth
 
 const app = express()
 
+app.set('query parser', (str) => qs.parse(str))
 app.use(express.urlencoded())
-
-console
 
 app.use(router)
 
