230d490cd06575734c1187d42286b2b1ed7c99df
dimastadeoo <dimastadeoo@gmail.com>
2026-07-27 14:16:15 +0700

    feat: add auth router with register and login endpoints

---
+++ src/routers/auth.router.js
@@ -0,0 +1,9 @@
+import {Router} from "express"
+import * as AuthController from "../controllers/auth.controller.js"
+
+const authRouter = Router()
+
+authRouter.post("/register", AuthController.register)
+authRouter.post("/login", AuthController.login)
+
+export default authRouter
