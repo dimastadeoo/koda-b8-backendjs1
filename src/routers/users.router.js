48223197df8661f96c5479d40236487224622c1c
dimastadeoo <dimastadeoo@gmail.com>
2026-07-28 14:54:55 +0700

    feat: add auth middleware and upload handling for user picture updates

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,11 +1,18 @@
 import {Router} from "express"
 import * as UserController from "../controllers/users.controller.js"
+import authMiddleware from "../middlewares/authMiddleware.js"
+// import { fileFilterImg } from "../lib/filterImg.js"
+// import { storagePictureUsers, limitImg } from "../lib/uploadPictureUsers.js"
+import { uploadMiddleware } from "../middlewares/upload.js"
 
 const userRouter = Router()
+
+userRouter.use(authMiddleware)
 
 userRouter.get("", UserController.getAll)
 userRouter.get("/:id", UserController.getById)
 userRouter.post("", UserController.createUser)
+userRouter.patch("/:id/picture",uploadMiddleware.single('picture'), UserController.uploadPicture)
 userRouter.patch("/:id", UserController.updateUser)
 userRouter.delete("/:id", UserController.deleteUser)
 
